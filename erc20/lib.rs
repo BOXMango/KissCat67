15fa6856ededfa8ed18ffaa96e552a9881b61e11
BOXMango <94897284+BOXMango@users.noreply.github.com>
2022-05-09 16:48:52 +0800

    Update lib.rs

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -133,5 +133,83 @@ mod erc20 {
         pub fn allowance(&self, owner:AccountId, spender:AccountId) ->u64 {
             self.allowance_of_or_zero(&owner, &spender)
         }
+        #[ink(message)]
+        pub fn allowance(&self, owner:AccountId, spender:AccountId) ->u64 {
+            self.allowance_of_or_zero(&owner, &spender)
+        }
+        #[ink(message)]
+        pub fn transfer(&mut self , to:AccountId, value:u64) ->bool {
+            let from = self.env().caller();
+            self.transfer_from_to(from, to, value)
+        }
+        #[ink(message)]
+        pub fn approve(&mut self , spender: AccountId,value: u64) ->bool{
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value );
+            self.env().emit_event(Approval{
+                owner,
+                spender,
+                value ,
+            });
+            true
+        }
+        
        
+
+        #[ink(message)]
+        pub fn mint_token_by_owner(&mut self, to:AccountId, value:u64, ) ->bool {
+            let caller = self.env().caller();
+            assert_eq!(caller == self.owner,true);
+            self._mint_token(to, value )
+        }
+       
+        #[ink(message)]
+        pub fn destroy_token_by_owner(&mut self ,from:AccountId,value:u64,) ->bool {
+            assert_eq!(value > 0 , true);
+            self._destroy_token(from,value)
+        }
+
+        
+        
+        fn _mint_token(
+            &mut self,
+            to:AccountId,
+            amount:u64,
+        ) -> bool {
+            let total_supply = self.total_supply();
+            assert_eq!(total_supply + amount >= total_supply, true);
+            let to_balance = self.balance_of_or_zero(&to);
+            assert_eq!(to_balance+amount >= to_balance,true);
+            self.total_supply += amount;
+            self.balances.insert(to,to_balance+amount);
+            self.env().emit_event(Transfer{
+                from:None,
+                to:Some(to),
+                value:amount,
+            });
+            true
+        }
+       
+
+        fn _destroy_token(
+            &mut self,
+            from:AccountId,
+            amount:u64,
+        ) -> bool{
+            let total_supply = self.total_supply();
+            assert_eq!(total_supply - amount <= total_supply, true);
+            let from_balance = self.balance_of_or_zero(&from);
+            assert_eq!(from_balance - amount <= from_balance,true);
+            self.total_supply -= amount;
+            self.balances.insert(from,from_balance -amount);
+            self.env().emit_event(Transfer{
+                from:Some(from),
+                to:None,
+                value:amount,
+
+            });
+            true
+                
+            }
+        }
     }
