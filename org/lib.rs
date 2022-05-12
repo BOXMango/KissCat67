f5b1158526dd681cc66688070f9ea3d23fc65f1d
BOXMango <94897284+BOXMango@users.noreply.github.com>
2022-05-12 17:58:55 +0800

    Update lib.rs

--- org/lib.rs
+++ org/lib.rs
@@ -613,7 +613,24 @@ mod org {
 
         }
 
-       
+        #[ink::test]
+        fn remove_moderator_works() {
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                    .expect("Cannot get accounts");
+            // Create a new contract instance.
+            let mut org_manager = OrgManager::new(accounts.alice,1, accounts.alice);
+            let bob_name = String::from("bob");
+            org_manager.add_dao_moderator(bob_name,accounts.bob);
+            org_manager.remove_dao_moderator(accounts.bob);
+
+            let mut members = org_manager.get_dao_moderator_list();
+            assert_eq!(members.len(), 0);
+
+        }
+
+
+      
         
     }
         
